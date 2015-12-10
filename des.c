80482f9a29e676cbfb5289f2df0278ca799a8e8b
djech <jerome.roessler@gmail.com>
2015-12-10 11:44:03 +0100

    expansion d'un bloc

--- des.c
+++ des.c
@@ -1,5 +1,14 @@
 //ctrl+shift+h
 #include "des.h"
+int expansion[48] = {
+  32,1,2,3,4,5,
+  4,5,6,7,8,9,
+  8,9,10,11,12,13,
+  12,13,14,15,16,17,
+  16,17,18,19,20,21,
+  20,21,22,23,24,25,
+  24,25,26,27,28,29,
+  28,29,30,31,32,1};
 
 char* subArray(char* array, int index, int length)
 {
@@ -9,6 +18,19 @@ char* subArray(char* array, int index, int length)
     for(i = 0; i <= length; i++)
     {
         res[i + length] = array[index + i];
+    }
+
+    return res;
+}
+
+unsigned char* expansionArray(unsigned char* array, int length)
+{
+    unsigned char* res = malloc(sizeof(unsigned char) * length);
+    int i;
+
+    for(i = 0; i <= length; i++)
+    {
+        res[i] = array[expansion[i]];
     }
 
     return res;
